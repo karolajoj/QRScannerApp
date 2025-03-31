5fd8a45f46c5b81a7fe1941ceaa4bfb0a56537b6
Karol Kowalczyk <karolajoj@gmail.com>
2025-03-31 18:59:23 +0200

    Chore: update dependencies to latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.qrscannerapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.qrscannerapp"
